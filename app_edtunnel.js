72c82ea3159e79fb61474768338c727df681693a
first879 <138953584+first879@users.noreply.github.com>
2023-07-27 19:41:02 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'ed5aba96-06f2-48a7-ad4d-27cf32415b3a';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
